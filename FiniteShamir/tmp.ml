ebe2604958237709b430764821b5128366ed9fcb
Jared Faucher <faucher@g.harvard.edu>
2015-04-23 21:35:02 -0400

    working on completing the reconstruction phase. mod aritmetic is not working for negatives and need to fix scale_denom

--- FiniteShamir/tmp.ml
+++ FiniteShamir/tmp.ml
@@ -1,45 +1,21 @@
-open Core.Std
-
-module type SHAMIR =
-sig
-  type secret
-  type poly
-  type key
-end
-
-module type SHAMIR_DECODE =
-sig
-  include SHAMIR
-  type lagrange_poly
-  val int_int_to_key: (int*int) list -> key list
-  val get_secret: int -> key list -> int
-end
-
-module FiniteShamirInt_decode =
-struct
-  type secret = int
-  type key = int * int
-  type poly = int list
-  type lagrange_poly = int * poly
-  
-  let rec int_int_to_key (lst: (int*int) list) : key list =
+  let rec int_int_to_key (lst: (int*int) list) : (int * int) list =
     match lst with
     | [] -> []
     | h::t -> h::(int_int_to_key t)
   ;;
 
   (* decoding functions *)
-  let get_key_x (key: key) : int =
+  let get_key_x (key: (int * int)) : int =
     fst(key)
   ;;
 
-  let get_key_y (key: key) : int =
+  let get_key_y (key: (int * int)) : int =
     snd(key)
   ;;
 
   (* adds two polys together *)
-  let add_polys (x:poly) (y:poly) : poly =
-    let rec helper (x:poly) (y:poly) (acc:poly) : poly =
+  let add_polys (x:(int list)) (y:(int list)) : (int list) =
+    let rec helper (x:(int list)) (y:(int list)) (acc:(int list)) : (int list) =
       match (x, y) with
       | ([],[]) -> acc
       | ([], yh::yt) -> helper [] yt (yh::acc)
@@ -49,21 +25,21 @@ struct
     in List.rev (helper x y [])
   ;;
 
-  let neg_poly (poly:poly) : poly =
+  let neg_poly (poly:(int list)) : (int list) =
     List.map ~f:(fun x -> (-1) * x) poly
   ;;
 
-  let mult_poly_int (x:int) (poly:poly) : poly =
+  let mult_poly_int (x:int) (poly:(int list)) : (int list) =
     List.map ~f:(fun a -> a * x) poly
   ;;
 
-  let div_poly_int (x:int) (poly:poly) : poly =
+  let div_poly_int (x:int) (poly:(int list)) : (int list) =
     List.map ~f:(fun a -> a / x) poly
   ;;
 
   (* Used to mod each coeff in a poly by some int
    * x *)
-  let mod_poly_elts (x: int) (p: poly) : poly =
+  let mod_poly_elts (x: int) (p: (int list)) : (int list) =
     List.map ~f:(fun a -> 
       if a >= 0 then a mod x
       else x - (-a mod x)) p
@@ -91,34 +67,34 @@ struct
   ;;
 
   (* multiplies a poly by (x + a) *)
-  let mult_x_a_poly (a: int) (poly: poly) : poly =
+  let mult_x_a_poly (a: int) (poly: (int list)) : (int list) =
     let x_half = [0] @ poly in
     let a_half = mult_poly_int a poly in
     add_polys x_half a_half
   ;;
 
-  let gen_lagrange_denom (x:int) (keys: key list) : int =
+  let gen_lagrange_denom (x:int) (keys: (int * int) list) : int =
     let filtered_keys = List.filter ~f:(fun k -> (get_key_x k) <> x) keys in
     let filtered_keys_xs = List.map ~f:(get_key_x) filtered_keys in
     let denom = List.map ~f:(fun a -> x - a) filtered_keys_xs in
     List.fold_left ~f:( * ) ~init:1 denom
   ;;
 
-  let gen_lagrange_num (x:int) (keys: key list) : poly =
+  let gen_lagrange_num (x:int) (keys: (int * int) list) : (int list) =
     let filtered_keys = List.filter ~f:(fun k -> (get_key_x k) <> x) keys in
     let neg_filtered_keys_xs = 
       List.map ~f:(fun k -> -1*(get_key_x k)) filtered_keys in
     List.fold_right ~f:mult_x_a_poly ~init:[1] neg_filtered_keys_xs
   ;;
 
-  let gen_lagrange_poly (key: key) (keys: key list): lagrange_poly =
+  let gen_lagrange_poly (key: (int * int)) (keys: (int * int) list): (int * (int list)) =
     let x = get_key_x key in
     let denom = gen_lagrange_denom x keys in
     let num = gen_lagrange_num x keys in
     (denom, num)
   ;;
 
-  let gen_lag_poly_list (keys: key list) : lagrange_poly list =
+  let gen_lag_poly_list (keys: (int * int) list) : (int * (int list)) list =
     List.map ~f:(fun x -> gen_lagrange_poly x keys) keys
   ;;
 
@@ -127,20 +103,17 @@ struct
    * help us avoid division errors when calculating our secret.
    * E.G. if our 3 lag polys are (1, [1;2;3]), (2,[4;5;6]) and
    * (3,[7;8;9]), our new denominator would be 1*2*3 or 6. *)
-  let rec scale_denoms (lags: lagrange_poly list) (accum: int) : int =
+  let rec scale_denoms (lags: (int * (int list)) list) (accum: int) : int =
     match lags with
     | [] -> accum
     | (x, _)::tl ->
       scale_denoms tl (x * accum)
   ;;
-
-  (* NEED TO CHANGE SCALE_DENOMS TO FIND COMMON DENOM *)
-
   (* This scales all our lagrange polynomial based on the denominator
    * d provided.  From the example in the previous function's comments
    * for (2,[4;5;6]) our resulting lag_poly from the denominator 6 would
    * be (6, [12;15;18]), where each coeff is scaled by a factor of 3. *)
-  let scale_lag_poly (lag: lagrange_poly) (d: int) : lagrange_poly =
+  let scale_lag_poly (lag: (int * (int list))) (d: int) : (int * (int list)) =
     match lag with
     | (x, l) ->
       let scale = d / x in
@@ -151,11 +124,11 @@ struct
    * would become (6,[6;12;18]), (6,[12;15;18]) and (6,[14;16;18]). This will
    * be useful in calculating our secret to help us avoid integer division
    * errors. *)
-  let scale_lag_polys (lags: lagrange_poly list) (d: int) : lagrange_poly list =
+  let scale_lag_polys (lags: (int * (int list)) list) (d: int) : (int * (int list)) list =
     List.map ~f:(fun x -> scale_lag_poly x d) lags
   ;;
 
-  let rec combine_lag_ys (ys: int list) (lags: lagrange_poly list) : poly list =
+  let rec combine_lag_ys (ys: int list) (lags: (int * (int list)) list) : (int list) list =
     match ys, lags with
     | [],[] -> []
     | yhd::ytl, laghd::lagtl ->
@@ -167,7 +140,7 @@ struct
   
 
   (* FINISH DECODE_KEYS WITH PRIME NUMBER AND MODULO FUNCTIONS *)
-  let decode_keys (p: int) (keys: key list) : poly =
+  let decode_keys (p: int) (keys: (int * int) list) : (int list) =
     let lag_polys = gen_lag_poly_list keys in
     let denom = scale_denoms lag_polys 1 in
     let scaled_lags = scale_lag_polys lag_polys denom in
@@ -179,47 +152,8 @@ struct
     mod_poly_elts p num_2
   ;;
   
-  let get_secret (prime: int) (keys: key list) : int =
+  let get_secret (prime: int) (keys: (int * int) list) : int =
     match decode_keys prime keys with
     | h::_ -> h
     | _ -> failwith "broken"
   ;;
-end
- 
-module FiniteShamirIntDecode = (FiniteShamirInt_decode : SHAMIR_DECODE)
-
-let rec try_read_int () =
-  try read_int () with
-    Failure _ -> 
-      print_string "\nPlease enter an integer value: ";
-      try_read_int ()
-  ;;
-
-let rec get_key_cl (count: int) (accum: (int * int) list) : (int * int) list =
-  if count <= 0 then accum
-  else let () = print_string "\nEnter key x: " in
-       let x = try_read_int () in
-       let () = print_string "\nEnter key y: " in
-       let y = try_read_int () in
-       get_key_cl (count - 1) ((x,y)::accum)
-	 ;; 
-
-let initialize () =
-  let () = print_string "\nSHAMIR'S SECRET SHARING SCHEME: Initialization Process...
-    \nGive me the prime number integer: " in
-  let prime = try_read_int () in
-  let () = print_string "\nGive me the threshold value: " in
-  let threshold = try_read_int () in
-   (prime, (get_key_cl threshold []))
-  
-
-let main () =
-  let (prime, keys) = initialize () in
-  let secret = FiniteShamirIntDecode.get_secret 
-    (prime)
-    (FiniteShamirIntDecode.int_int_to_key keys) in
-  Printf.printf "secret: %i\n" secret
-;;
-
-main ();;
-
